ffabbf4affbc000c4121c377883c1fe7a86b0d0d
blydo <Romabrykov2010@yandex.ru>
2025-06-07 15:27:12 +0500

    урапааа

--- Project23/main.cpp
+++ Project23/main.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 #include <string>
+#include <Windows.h>
+#include <iomanip>
 
+void AddProduct();
 void ChangePrice();
 void ShowTheWarehouse();
 void ShowMenu();
+void ChangeAmounth();
+void ChangeTitle();
+void RemoveProduct();
+void ChangeAssortment();
 
 int size = 10;
-std::string* title = new std::string[size]{ "чизбергер", "гамбургер", "черный лари", "сырная скала", "джейсон стетхам", "доминик торрето", "острый пукан", "бурш калифа", "травка по скидке", "сосугавкус" };
-int* prices = new int[size] {100, 90, 759, 940, 1000, 1500, 568, 2500, 300, 825 };
+std::string* Title = new std::string[size]{ "чизбергер", "гамбургер", "черный лари", "сырная скала", "джейсон стетхам", "доминик торрето", "острый пукан", "бурш калифа", "травка по скидке", "сосугавкус" };
+int* Price = new int[size] {100, 90, 759, 940, 1000, 1500, 568, 2500, 300, 825 };
 int* Amounth = new int[size] {50, 70, 20, 10, 20, 90, 40, 20, 15, 25 };
 
 int main() 
 {
-	setlocale(LC_ALL, "ru");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	ShowMenu();
+
 
 
 	
@@ -29,7 +38,7 @@ void ShowMenu()
 	std::cout << "1. начать покупки\n";
 	std::cout << "2. изменить цены\n";
 	std::cout << "3. изменить количество продуктов\n";
-	std::cout << "4. изменение смортимента\n";
+	std::cout << "4. изменение ассортимента\n";
 	std::cout << "5. изменение названия продуктов\n";
 	std::cout << "6. Показать склад\n";
 	std::cout << "7. выход\n";
@@ -47,15 +56,15 @@ void ShowMenu()
 	}
 	else if (choose == "3")
 	{
-
+		ChangeAmounth();
 	}
 	else if (choose == "4")
 	{
-
+		ChangeAssortment();
 	}
 	else if (choose == "5")
 	{
-
+		ChangeTitle();
 	}
 	else if (choose == "6")
 	{
@@ -76,29 +85,110 @@ void ChangePrice()
 	ShowTheWarehouse();
 	int choose;
 	int newPrice;
-	std::cout << "ввод";
+	std::cout << "ввод: ";
 	std::cin >> choose;
 	
 	system("cls");
 	std::cout << "Введите новую цену: ";
 	std::cin >> newPrice;
-	prices[choose - 1] = newPrice;
+	Price[choose - 1] = newPrice;
 
 	ShowMenu();
 
 }
+void ChangeAmounth() 
+{
+	ShowTheWarehouse();
+	int choose;
+	int newAmounth;
+	std::cout << "ввод: ";
+	std::cin >> choose;
+
+	system("cls");
+	std::cout << "введите количество: ";
+	std::cin >> newAmounth;
+	Amounth[choose - 1] = newAmounth;
+
+	ShowMenu();
+}
+void ChangeTitle() 
+{
+	ShowTheWarehouse();
+	int choose;
+	std::string newTitle;
+	std::cout << "ввод: ";
+	std::cin >> choose;
+
+	system("cls");
+	std::cout << "введите новое название: ";
+	std::cin >> newTitle;
+	Title[choose - 1] = newTitle;
+	ShowMenu();
+
+}
+void ChangeAssortment() 
+{
+	system("cls");
+	int choose{};
+	std::cout << "1. Удалить продукт\n";
+	std::cout << "2. Добавить продукт\n";
+	std::cout << "Ввод: ";
+	std::cin >> choose;
+	if (choose == 1)
+	{
+		RemoveProduct();
+	}
+	else if (choose == 2)
+	{
+		AddProduct();
+	}
+
+
+	ShowMenu();
+}
+void RemoveProduct() 
+{
+	int choose{};
+
+	ShowTheWarehouse();
+	std::cout << "\nВведите номер продукта: ";
+	std::cin >> choose;
+
+	std::string* tmpTitle = new std::string[size];
+	int* tmpPrice = new int[size];
+	int* tmpAmounth= new int[size];
+
+	for (int i = 1; i < size; i++)
+	{
+		if (i == choose)
+		{
+			tmpTitle[i] = " ";
+			tmpPrice[i] = 0;
+			tmpAmounth[i] = 0;
+		}
+			tmpTitle[i] = Title[i];
+			tmpPrice[i] = Price[i];
+			tmpAmounth[i] = Amounth[i];
+		
+		
+	}
+	std::swap(tmpTitle, Title);
+	std::swap(tmpPrice, Price);
+	std::swap(tmpAmounth, Amounth);
+	
+
+}
+void AddProduct() 
+{
+
+}
+
 
 void ShowTheWarehouse() 
 {
 	system("cls");
-	std::cout << "\n\n1. Название: " << title[0] << " кол-во: " << Amounth[0] << " цена: " << prices[0] << "\n";
-	std::cout << "2. Название: " << title[1] << " кол-во: " << Amounth[1] << " цена: " << prices[1] << "\n";
-	std::cout << "3. Название: " << title[2] << " кол-во: " << Amounth[2] << " цена: " << prices[2] << "\n";
-	std::cout << "4. Название: " << title[3] << " кол-во: " << Amounth[3] << " цена: " << prices[3] << "\n";
-	std::cout << "5. Название: " << title[4] << " кол-во: " << Amounth[4] << " цена: " << prices[4] << "\n";
-	std::cout << "6. Название: " << title[5] << " кол-во: " << Amounth[5] << " цена: " << prices[5] << "\n";
-	std::cout << "7. Название: " << title[6] << " кол-во: " << Amounth[6] << " цена: " << prices[6] << "\n";
-	std::cout << "8. Название: " << title[7] << " кол-во: " << Amounth[7] << " цена: " << prices[7] << "\n";
-	std::cout << "9. Название: " << title[8] << " кол-во: " << Amounth[8] << " цена: " << prices[8] << "\n";
-	std::cout << "10. Название: " << title[9] << " кол-во: " << Amounth[9] << " цена: " << prices[9] << "\n";
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << i + 1 << '\t' << std::left << std::setw(30) << Title[i] << "\t" << Amounth[i] << "\t" << Price[i] << "\n";
+	}
 }
