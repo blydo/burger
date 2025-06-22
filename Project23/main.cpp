0e6b1e8f8cd72e2f9dd0b186291e012ec7d69a59
blydo <Romabrykov2010@yandex.ru>
2025-06-22 15:07:38 +0500

    validation

--- Project23/main.cpp
+++ Project23/main.cpp
@@ -35,52 +35,61 @@ int main()
 
 void ShowMenu() 
 {
-	system("cls");
-	std::string choose;
-	std::cout << "бургерная харамчика\n\n";
-	std::cout << "1. начать покупки\n";
-	std::cout << "2. изменить цены\n";
-	std::cout << "3. изменить количество продуктов\n";
-	std::cout << "4. изменение ассортимента\n";
-	std::cout << "5. изменение названия продуктов\n";
-	std::cout << "6. Показать склад\n";
-	std::cout << "7. выход\n";
-	std::cout << "\n\nВвод: ";
+	while (true)
+	{
+		system("cls");
+		std::string choose;
+		std::cout << "бургерная харамчика\n\n";
+		std::cout << "1. начать покупки\n";
+		std::cout << "2. изменить цены\n";
+		std::cout << "3. изменить количество продуктов\n";
+		std::cout << "4. изменение ассортимента\n";
+		std::cout << "5. изменение названия продуктов\n";
+		std::cout << "6. Показать склад\n";
+		std::cout << "7. выход\n";
+		std::cout << "\n\nВвод: ";
 
-	std::cin >> choose;
-	if (choose == "1")
-	{
-		StartSells();
-	}
-	else if (choose == "2")
-	{
-		ChangePrice();
+		std::cin >> choose;
+		if (std::stoi(choose) <= 7 && std::stoi(choose) < 1)
+		{
+			std::cout << "неправильный ввод";
+		}
 
-	}
-	else if (choose == "3")
-	{
-		ChangeAmounth();
-	}
-	else if (choose == "4")
-	{
-		ChangeAssortment();
-	}
-	else if (choose == "5")
-	{
-		ChangeTitle();
-	}
-	else if (choose == "6")
-	{
-		std::string str;
-		ShowTheWarehouse();
-		std::cout << "\n\nВведите что угодно для выхода: ";
-		std::cin >> str;
-		ShowMenu();
 
-	}
-	else if (choose == "7")
-	{
-		exit(0);
+		if (choose == "1")
+		{
+			StartSells();
+		}
+		else if (choose == "2")
+		{
+			ChangePrice();
+
+		}
+		else if (choose == "3")
+		{
+			ChangeAmounth();
+		}
+		else if (choose == "4")
+		{
+			ChangeAssortment();
+		}
+		else if (choose == "5")
+		{
+			ChangeTitle();
+		}
+		else if (choose == "6")
+		{
+			std::string str;
+			ShowTheWarehouse();
+			std::cout << "\n\nВведите что угодно для выхода: ";
+			std::cin >> str;
+			ShowMenu();
+
+		}
+		else if (choose == "7")
+		{
+			exit(0);
+		}
 	}
 }
 void StartSells() 
@@ -102,8 +111,20 @@ void StartSells()
 		{
 			break;
 		}
-		std::cout << "введите количество: ";
-		std::cin >> count;
+		
+		
+		while (true)
+		{
+			std::cout << "введите количество: ";
+			std::cin >> count;
+			if (count > Amounth[choose - 1])
+			{
+				std::cout << "отсоси у тебя слишком большой\n";
+				Sleep(2000);
+;				continue;
+			}
+			break;
+		}
 		if (count == 0)
 		{
 			continue;
